c4d09a805b3b61a41459f7e58ce145984f7a7d63
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-11-22 09:56:06 +0200

    change-task6

--- js/task-6.js
+++ js/task-6.js
@@ -38,3 +38,47 @@
 // function onDestroyBtnClick() {
 //   boxesRef.innerHTML = '';
 // }
+
+
+
+////Добавление с удалением////
+
+
+const inputEl = document.querySelector('#controls input');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', onAddsElements);
+btnDestroyEl.addEventListener('click', onClearsElements);
+
+function onAddsElements() {
+  if (inputEl.value === '' || inputEl.value < 1 || inputEl.value > 100) {
+    return;
+  }
+
+  let size = 20;
+  const arrElements = [];
+
+  for (let i = 0; i < inputEl.value; i++) {
+    size += 10;
+
+    arrElements.push(
+      `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px;"></div>`,
+    );
+  }
+
+  boxesEl.innerHTML = arrElements.join('');
+  inputEl.value = '';
+}
+
+function onClearsElements() {
+  boxesEl.innerHTML = '';
+  inputEl.value = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
