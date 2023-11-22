ca9b6f8d7a3341089e344da3a38d19aaf006ee54
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-11-22 14:13:50 +0200

    task6-save-enter-number

--- js/task-6.js
+++ js/task-6.js
@@ -44,10 +44,55 @@
 ////Добавление с удалением////
 
 
+// const inputEl = document.querySelector('#controls input');
+// const btnCreateEl = document.querySelector('[data-create]');
+// const btnDestroyEl = document.querySelector('[data-destroy]');
+// const boxesEl = document.querySelector('#boxes');
+
+// btnCreateEl.addEventListener('click', onAddsElements);
+// btnDestroyEl.addEventListener('click', onClearsElements);
+
+// function onAddsElements() {
+//   if (inputEl.value === '' || inputEl.value < 1 || inputEl.value > 100) {
+//     return;
+//   }
+
+//   let size = 20;
+//   const arrElements = [];
+
+//   for (let i = 0; i < inputEl.value; i++) {
+//     size += 10;
+
+//     arrElements.push(
+//       `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px;"></div>`,
+//     );
+//   }
+
+//   boxesEl.innerHTML = arrElements.join('');
+//   inputEl.value = '';
+// }
+
+// function onClearsElements() {
+//   boxesEl.innerHTML = '';
+//   inputEl.value = '';
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+
+/// Сохраняем число, что бы каждый раз не вводить одно и то же ////
+
 const inputEl = document.querySelector('#controls input');
 const btnCreateEl = document.querySelector('[data-create]');
 const btnDestroyEl = document.querySelector('[data-destroy]');
 const boxesEl = document.querySelector('#boxes');
+const numberDisplayEl = document.querySelector('#number-display');
+
+let numberOfElements = 0;
 
 btnCreateEl.addEventListener('click', onAddsElements);
 btnDestroyEl.addEventListener('click', onClearsElements);
@@ -69,12 +114,20 @@ function onAddsElements() {
   }
 
   boxesEl.innerHTML = arrElements.join('');
+  numberOfElements = inputEl.value; 
+  updateNumberDisplay(); 
   inputEl.value = '';
 }
 
 function onClearsElements() {
   boxesEl.innerHTML = '';
+  numberOfElements = 0; 
+  updateNumberDisplay(); 
   inputEl.value = '';
+}
+
+function updateNumberDisplay() {
+  numberDisplayEl.textContent = `Number of elements: ${numberOfElements}`;
 }
 
 function getRandomHexColor() {
